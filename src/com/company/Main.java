e518bd4edb1613de4e6512a5674fd79da61664ca
Adithya12121992 <adityaa.aditya.krishna@gmail.com>
2019-10-09 12:54:20 -0400

    Working code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,41 +2,24 @@ package com.company;
 import com.company.prioritizer1;
 
 import java.util.Scanner;
-//import com.company.prioritizer1_array;
+//import com.company.prioritizer1Array;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        prioritizer1 newOBJ = new  prioritizer1();
-        /*newOBJ.sizeOfStack = 10;
-        newOBJ.isInInsertionPhase();
-        newOBJ.insert(16);
-        newOBJ.insert(10);
-        newOBJ.insert(12);
-        newOBJ.insert(1991);
-        newOBJ.insert(110);
-        newOBJ.insert(12222);
-        newOBJ.insert(161);
-        newOBJ.insert(102);
-        newOBJ.insert(123);
-        newOBJ.insert(116);
-        newOBJ.insert(1330);
-        newOBJ.insert(121);
-        newOBJ.displayElements();
-        newOBJ.removeNextInOrder();
-        newOBJ.displayElements();
-        newOBJ.removeAny();
-        newOBJ.displayElements();
-        newOBJ.removeAny();
-        newOBJ.displayElements();
-        newOBJ.removeAny();
-        newOBJ.displayElements();*/
         System.out.println("Enter the size of the Prioritizer");
         Scanner scan = new Scanner(System.in);
-        newOBJ.sizeOfStack = scan.nextInt();
-        while (true){
-            System.out.println("Operations:\n1.)Insert\t2.)Remove in Ascending order\t3.)RemoveAny\t4.)CheckPhase\t5.)Exit");
+        prioritizer1 newOBJ = new  prioritizer1(scan.nextInt());
+        /*newOBJ.sizeOfStack();
+        newOBJ.insert(11.0);
+        newOBJ.insert(1.0);
+        newOBJ.insert(12.5);
+        newOBJ.insert(14.6);
+        newOBJ.removeNextInOrder();*/
+
+       while (true){
+            System.out.println("Operations:\n1.)Insert\t2.)Remove in Ascending order\t3.)RemoveAny\t4.)CheckPhase\t5.)Size of stack\t6.)Exit");
             int selection = scan.nextInt();
             switch (selection){
                 case 1:
@@ -56,7 +39,12 @@ public class Main {
                     newOBJ.isInInsertionPhase();
                     break;
                 case 5:
+                    newOBJ.sizeOfStack();
+                    break;
+                case 6:
                     System.exit(0);
+                default:
+                    System.out.println("Wrong entry, please try again");
             }
         }
     }
--- src/com/company/prioritizer_interface.java
+++ src/com/company/prioritizer_interface.java
@@ -5,6 +5,7 @@ interface prioritizerInterface<T> {
 
     default public void insert(T element){
             boolean isInInsertion=true; // setting the isInInsertion to true every time whenever we want to insert an element
+            System.out.println("Changed to the Insertion Phase");
     } //for addition of elements into the Stack
 
     public void changePhase(); //for changing the phase of the stack operation
@@ -13,7 +14,9 @@ interface prioritizerInterface<T> {
         changePhase(); //Calling the function ChangePhase to change the phase form InsertPhase
     }  //for removal of elements form the Stack
 
-    public void removeAny(); //for removal of elements form the Stack
+    default public void removeAny(){
+        changePhase(); //Calling the function ChangePhase to change the phase form InsertPhase
+    } //for removal of elements form the Stack
     public void sizeOfStack(); //for displaying the size of the Stack
     public void isInInsertionPhase(); //for getting the phase of the Stack
     public void displayElements(); //for displaying the elements
